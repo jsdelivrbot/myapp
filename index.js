dd8861e5c5ccc03dc9542d6d13f6cb325104f85a
rajendrapadiyar <30294680+rajendrapadiyar@users.noreply.github.com>
2018-02-05 06:33:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Node js sample app deployed successfully')
+  response.send('Node js sample app deployed successfully again')
 })
 
 app.listen(app.get('port'), function() {
